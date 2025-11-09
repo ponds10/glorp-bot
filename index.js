fab36611b9a38d2f5f607ffcf81aeac8bf27e196
alistair <alistairnds@gmail.com>
2025-11-09 15:45:00 -0800

    switch from CJS to ESM

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require("discord.js");
-const { token } = require("./config.json");
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import token from "./config.json";
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
